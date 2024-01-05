660128a8e3a681286fbea429b8b693c7d2577478
Ariful Islam <hello.mrariful@gmail.com>
2024-01-05 18:15:07 +0600

    projects setup with tailwindCSS

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="flex">
+        <img className="w-10 bg-white" src="test.png" alt="" />
+        <h1 className="text-white text-3xl"> Online Test</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
